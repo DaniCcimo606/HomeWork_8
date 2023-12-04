8589d938bd0798d6fc1d464ecc0cd80d4ea37c3e
DaniCcimo <bessmertnyj228@gmail.com>
2023-12-04 06:07:48 +0200

    Fixed code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
-function animation() {
-    let spans = document.querySelectorAll('span'); // Мог сделать через split, но... мне было впадлу.........
-    
-    spans.forEach(item => {
-        let randomTime = parseFloat(Math.random().toFixed(1));
-    
-        item.style.animation = `DaniCcimo ${randomTime}s infinite`;
-    });
+if (document.title == 'bonus') {
+    function DaniCcimo() {
+        let spans = document.querySelectorAll('span'); // Мог сделать через split, но... мне было впадлу.........
+        
+        spans.forEach(item => {
+            let randomTime = parseFloat(Math.random().toFixed(1));
+        
+            item.style.animation = `DaniCcimo ${randomTime}s infinite`;
+        });
+    }
+
+    setInterval (DaniCcimo, 700);
 }
 
-setInterval (animation, 700)
+
+if (document.title === 'index') {
+    let lightningWrap = document.querySelector('.lightning-wrap');
+    let isShow = true;
+
+    function toggleLightning() {
+        if (isShow) {
+            lightningWrap.classList.remove('lightningShow');
+            isShow = false;
+        } else {
+            lightningWrap.classList.add('lightningShow');
+            isShow = true;
+        }
+    }
+
+    setInterval(toggleLightning, 500);
+}
